dd0d3f0cbe83fc0ff91d09b1378631f0441af4a2
lim jiaying <kerrykerry123456789@hotmail.com>
2020-01-13 08:43:14 +0800

    Push for nicholas

--- app/src/main/java/com/example/jobx/chat/UserList.kt
+++ app/src/main/java/com/example/jobx/chat/UserList.kt
@@ -11,18 +11,18 @@ import com.google.firebase.firestore.FirebaseFirestore
 class UserList : AppCompatActivity() {
     private var listUser:List<User>?=null
     companion object {
-        val USER_KEY = "xrazZlNvMFc05tUAh2fplo9a0OV2"
+        val USER_KEY = ""
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_list)
         supportActionBar?.title = "Select User"
 //        fetchUser()
-        if(FirebaseAuth.getInstance().currentUser!=null) {
-            val intent = Intent(this, ChatLog::class.java)
-            intent.putExtra(USER_KEY, "xrazZlNvMFc05tUAh2fplo9a0OV2")
-            startActivity(intent)
-        }
+//        if(FirebaseAuth.getInstance().currentUser!=null) {
+//            val intent = Intent(this, ChatLog::class.java)
+//            intent.putExtra(USER_KEY, "xrazZlNvMFc05tUAh2fplo9a0OV2")
+//            startActivity(intent)
+//        }
     }
     private fun fetchUser(){
         val ref = FirebaseFirestore.getInstance().collection("users").whereEqualTo("status","true")
